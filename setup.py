39818b60544b9b6365ee659d49bee3ff0eb8e149
Amit Kushwaha <amit.2022ug1070@iiitranchi.ac.in>
2025-10-02 05:05:03 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    version= '0.0.1',
+    author='Amit Kushwaha',
+    author_email='amitkushwaha43714@gmail.com',
+    packages=find_packages()
+)

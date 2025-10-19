4a3e6a5ab422b7524d382129656b3f3978862ffa
Amit Kushwaha <amit.2022ug1070@iiitranchi.ac.in>
2025-10-20 04:20:16 +0530

    Model trained

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -1,7 +1,38 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path:str 
-    test_file_path:str
+    trained_file_path: str
+    test_file_path: str
+
+    
+
+class DataValidationArtifact:
+    def __init__(self, validation_status: bool, message: str, validation_report_file_path: str):
+        self.validation_status = validation_status
+        self.message = message
+        self.validation_report_file_path = validation_report_file_path
+
+    def __repr__(self):
+        return f"DataValidationArtifact(validation_status={self.validation_status}, message={self.message}, validation_report_file_path={self.validation_report_file_path})"
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path:str 
+    transformed_train_file_path:str
+    transformed_test_file_path:str
+
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+
+    
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str 
+    metric_artifact:ClassificationMetricArtifact
